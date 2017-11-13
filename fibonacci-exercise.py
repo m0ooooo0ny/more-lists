74037d0b11821ab46bab67c20da2fa6bc33dea20
Kat Xu <xuk@npsdnj.org>
2017-11-13 17:23:23 +0000

    finish fibonacci-exercise

--- fibonacci-exercise.py
+++ fibonacci-exercise.py
@@ -1,30 +1,35 @@
-"""
-A fibonacci sequence is a series of numbers in which each number is the sum of the two preceding numbers.
+"""A fibonacci sequence is a series of numbers in which each number is the sum of the two preceding numbers.
 Starting with 0 and 1, the first ten terms are: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
 
 Your task here is to write code that will fill in the list called 'fib' so that it contains
-the first 50 terms of the fibonacci sequence, starting with 0
+the first 50 terms of the fibonacci sequence, starting with 0"""
 
 
 # Don't try writing the code all at once. Break it down into pieces
 # I have some tips below that will help you figure this out. You can delete them (and the code that goes with them) when you no longer need them
-"""
+
 
 fib = [0, 1]
 
 # TIP: How can you refer to the last item in a list? Print the last item in the list called fib
+print(fib[-1])
 
 # TIP: How about the second to last item?
+print(fib[-2])
 
 # TIP: How could you use the two items you've already accessed to generate the next term? Print the next term of the fibonacci sequence
+print(fib[-1] + fib[-2])
 
 # TIP: Now that you've figured out how to generate the next item in your sequence, how would you add it to the end of your list
+fib.append(fib[-1] + fib[-2])
+print(fib[:])
 
 # TIP: You've gotten the next item in the sequence and added it to your list - now how do you repeat it?
 # There are two good options based on your current knowledge - you could repeat a certain number of times (what type of loop is that?)
 # or you could repeat as long as there are too few items in the list (what type of loop is that?)
 # You might want to refresh your memory by creating a simple loop of the type you want first
-
+for x in range(47):
+    fib.append(fib[-1] + fib[-2])
 
 
 
